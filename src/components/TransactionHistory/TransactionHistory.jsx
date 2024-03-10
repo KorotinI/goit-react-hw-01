9d20de6ed86579282a54242e6343c02ed8717bc8
KorotinI <142391798+KorotinI@users.noreply.github.com>
2024-03-10 19:35:40 +0100

    trhistory

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,39 +1,25 @@
-import css from '../TransactionHistory/TransactionHistory.module.css';
+import css from "./TransactionHistory.module.css"; // Додайте цей імпорт
 
-const TransactionHistory = ({ items }) => {
+export const TransactionHistory = ({ items }) => {
   return (
-    <table className={css.table}>
-      <thead className={css.tableTitles}>
-        <tr className={css.tableRow}>
-          <th className={css.tableTitle}>Type</th>
-          <th className={css.tableTitle}>Amount</th>
-          <th className={css.tableTitle}>Currency</th>
+    <table>
+      <thead>
+        <tr>
+          <th className={css.th}>Type</th>
+          <th className={css.th}>Amount</th>
+          <th className={css.th}>Currency</th>
         </tr>
       </thead>
-      <tbody className={css.tableBody}>
-        {items.map(item => {
-          return (
-            <tr className={css.tableRow} key={item.id}>
-              <Transaction
-                type={item.type}
-                amount={item.amount}
-                currency={item.currency}
-              />
-            </tr>
-          );
-        })}
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr className={css.tr} key={id}>
+            <td className={css.td}>{type}</td>
+            <td className={css.td}>{amount}</td>
+            <td className={css.td}>{currency}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
-  );
-};
-
-const Transaction = ({ type, amount, currency }) => {
-  return (
-    <>
-      <td className={css.tableDescrType}>{type}</td>
-      <td className={css.tableDescr}>{amount}</td>
-      <td className={css.tableDescr}>{currency}</td>
-    </>
   );
 };
 
