7a3fe461287449fcd1b8aec83cafdc0cf65df024
KorotinI <142391798+KorotinI@users.noreply.github.com>
2024-03-10 19:52:49 +0100

    fix bugs

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,19 +3,19 @@ import clsx from 'clsx';
 import styles from './FriendListItem.module.css';
 
 const FriendListItem = ({ friend }) => {
-  const { avatar, isOnline, name: username } = friend;
+  const { image, isActive, name: username } = friend;
   return (
     <li className={styles.friendListItem}>
       <div>
-        <img src={avatar} alt={username} className={styles.friendImage} />
+        <img src={image} alt={username} className={styles.friendImage} />
         <h2>{username}</h2>
         <p
           className={clsx({
-            [styles.userOnline]: isOnline,
-            [styles.userOffline]: !isOnline,
+            [styles.userActive]: isActive,
+            [styles.userUnactive]: !isActive,
           })}
         >
-          {isOnline ? 'Online' : 'Offline'}
+          {isActive ? 'Online' : 'Offline'}
         </p>
       </div>
     </li>
